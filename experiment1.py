36ed1a64a888bab2bb026772aac3acfd92744e15
Biel Cardona <bielcardona@gmail.com>
2017-12-13 15:02:32 +0100

    experiment 1 nows asks for ranges of phi, theta and lambda

--- experiment1.py
+++ experiment1.py
@@ -12,9 +12,17 @@ import shutil
 
 
 def experiment(data, folder):
-    lambda1 = float(data['lambda1'])
-    lambda2 = float(data['lambda2'])
-    lambdad = float(data['lambdadelta'])
+    phi1 = float(data['phi1']) + 0.000001 #0 + 0.0000001 # TODO: pq sumar?
+    phi2 = float(data['phi2']) + 0.000001 #0
+    phidelta = float(data['phidelta']) #0.1
+    theta1 = float(data['theta1']) + 0.000001 #0 + 0.0000001 # TODO: pq sumar?
+    theta2 = float(data['theta2']) + 0.000001 #0
+    thetadelta = float(data['thetadelta']) #0.1
+    number_of_rays = 10
+    aperture_collector = 1. * 1. * 1.0
+    lambda1 = float(data['lambda1']) # # 295.0
+    lambda2 = float(data['lambda2']) # # 810.5
+    lambdadelta = float(data['lambdadelta']) # # 0.5
     freecad_file = os.path.join(folder, data['freecad_file'])
     PV_file = os.path.join(folder, data['PV_file'])
 
@@ -31,23 +39,11 @@ def experiment(data, folder):
 
     sel = doc.Objects
     current_scene = raytrace.Scene(sel)
-    phi_ini = 0 + 0.0000001
-    phi_end = 0
-    phi_end = phi_end + 0.00001
-    phi_step = 0.1
-    theta_ini = 0 + 0.0000001
-    theta_end = 0
-    theta_end = theta_end + 0.00001
-    theta_step = 2
-    number_of_rays = 10
-    aperture_collector = 1. * 1. * 1.0
-    lambda_ini = lambda1 # 295.0
-    lambda_end = lambda2 # 810.5
-    step_lambda = lambdad # 0.5
     ##### data_file_spectrum = 'ASTMG173-direct.txt'
     # light_spectrum = raytrace.create_CDF_from_PDF(data_file_spectrum)
     # Buie_model = raytrace.BuieDistribution(0.05)
     Buie_model = None
+
     destfolder = folder + '.output'
     os.makedirs(destfolder)
     outfile = open(os.path.join(destfolder,'kkk4.txt'), 'w')
@@ -56,33 +52,32 @@ def experiment(data, folder):
     outfile_Source_lambdas = open(os.path.join(destfolder,'Source_lambdas_1micro.txt'), 'w')
     outfile_Source_lambdas.write("%s %s" % (aperture_collector * 0.001 * 0.001, "# Collector aperture in m2") + '\n')
     outfile_Source_lambdas.write("%s %s" % (number_of_rays, "# Rays per wavelength") + '\n')
-    outfile_Source_lambdas.write("%s %s" % (step_lambda, "# Step of wavelength in nm") + '\n')
+    outfile_Source_lambdas.write("%s %s" % (lambdadelta, "# Step of wavelength in nm") + '\n')
 
     Source_lambdas = []
     PV_energy = []
     PV_wavelength = []
     PV_values = []
     t0 = time.time()
-    for ph in np.arange(phi_ini, phi_end, phi_step):
-        #    for th in np.arange(theta_ini, theta_end, theta_step):
-        for w in np.arange(lambda_ini, lambda_end, step_lambda):
-            th = theta_ini
-            light_spectrum = w
-            Source_lambdas.append(w)
-            main_direction = raytrace.polar_to_cartesian(ph, th) * -1.0  # Sun direction vector
-            emitting_region = raytrace.SunWindow(current_scene, main_direction)
-            l_s = raytrace.LightSource(current_scene, emitting_region, light_spectrum, 1.0, Buie_model)
-            exp = raytrace.Experiment(current_scene, l_s, number_of_rays)
-
-            exp.run()
-            efficiency = (exp.captured_energy / aperture_collector) / (
-                    exp.number_of_rays / exp.light_source.emitting_region.aperture)
-            t1 = time.time()
-            # print ("%s %s %s %s" % (w, th, efficiency, t1 - t0) + '\n')
-            outfile.write("%s %s %s %s" % (ph, th, efficiency, t1 - t0) + '\n')
-            PV_energy.append(exp.PV_energy)
-            PV_wavelength.append(exp.PV_wavelength)
-            PV_values.append(exp.PV_values)
+    for ph in np.arange(phi1, phi2, phidelta):
+        for th in np.arange(theta1, theta2, thetadelta):
+            for w in np.arange(lambda1, lambda2, lambdadelta):
+                light_spectrum = w
+                Source_lambdas.append(w)
+                main_direction = raytrace.polar_to_cartesian(ph, th) * -1.0  # Sun direction vector
+                emitting_region = raytrace.SunWindow(current_scene, main_direction)
+                l_s = raytrace.LightSource(current_scene, emitting_region, light_spectrum, 1.0, Buie_model)
+                exp = raytrace.Experiment(current_scene, l_s, number_of_rays)
+    
+                exp.run()
+                efficiency = (exp.captured_energy / aperture_collector) / (
+                        exp.number_of_rays / exp.light_source.emitting_region.aperture)
+                t1 = time.time()
+                # print ("%s %s %s %s" % (w, th, efficiency, t1 - t0) + '\n')
+                outfile.write("%s %s %s %s" % (ph, th, efficiency, t1 - t0) + '\n')
+                PV_energy.append(exp.PV_energy)
+                PV_wavelength.append(exp.PV_wavelength)
+                PV_values.append(exp.PV_values)
 
     xarray = np.array(np.concatenate(PV_energy))
     yarray = np.array(np.concatenate(PV_wavelength))
