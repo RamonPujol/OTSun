f718751f8355309a787a4e5c59026bfd2adf10d1
Biel Cardona <bielcardona@gmail.com>
2017-12-14 10:45:43 +0100

    multiprocessing is working

--- experiment1par.py
+++ experiment1par.py
@@ -13,44 +13,55 @@ import multiprocessing
 
 logging.getLogger().setLevel(logging.DEBUG)
 
-Source_lambdas = []
-PV_energy = []
-PV_wavelength = []
-PV_values = []
-t0 = time.time()
-Buie_model = None
+finished_computations_counter = None
+
+
+def init_counter(args):
+    ''' store the counter for later use '''
+    global finished_computations_counter
+    finished_computations_counter = args
+
+
+def computeX(args):
+    ph, th, w, number_of_rays, aperture_collector = args
+    logging.debug("experiment1X: %s, %s, %s", ph, th, w)
+    global finished_computations_counter
+    with finished_computations_counter.get_lock():
+        finished_computations_counter.value += 1
+    logging.debug('finished %s of %s computations', finished_computations_counter.value, total_computations)
+    return (ph, th, w, 0, 0, 0, 0)
 
 
 def compute(args):
+    # get parameters
     ph, th, w, number_of_rays, aperture_collector = args
-    logging.debug("experiment1: %s, %s, %s", ph, th, w)
-    light_spectrum = w
-    Source_lambdas.append(w)
+    logging.debug("running experiment1 with: ph=%s, th=%s, w=%s", ph, th, w)
+
+    # prepare experiment
     main_direction = raytrace.polar_to_cartesian(ph, th) * -1.0  # Sun direction vector
     emitting_region = raytrace.SunWindow(current_scene, main_direction)
-    l_s = raytrace.LightSource(current_scene, emitting_region, light_spectrum, 1.0, Buie_model)
+    l_s = raytrace.LightSource(current_scene, emitting_region, w, 1.0, None)
     exp = raytrace.Experiment(current_scene, l_s, number_of_rays)
 
+    # run experiment and compute output
     exp.run()
     efficiency = (exp.captured_energy / aperture_collector) / (
             exp.number_of_rays / exp.light_source.emitting_region.aperture)
-    t1 = time.time()
-    # print ("%s %s %s %s" % (w, th, efficiency, t1 - t0) + '\n')
-    #### outfile.write("%s %s %s %s" % (ph, th, efficiency, t1 - t0) + '\n')
-    PV_energy.append(exp.PV_energy)
-    PV_wavelength.append(exp.PV_wavelength)
-    PV_values.append(exp.PV_values)
+
+    # update the number of finished computations
+    global finished_computations_counter
+    with finished_computations_counter.get_lock():
+        finished_computations_counter.value += 1
+    logging.debug('finished %s of %s computations', finished_computations_counter.value, total_computations)
+
+    # return the results
     return (ph, th, w, efficiency, exp.PV_energy, exp.PV_wavelength, exp.PV_values)
-
-
-def process_computation(result):
-    logging.debug("processing result %s", result)
-    (ph, th, w, efficiency, PV_energy, PV_wavelength, PV_values) = result
 
 
 def experiment(data, folder):
     global current_scene
 
+    # get parameters of the experiment from the data dict
     phi1 = float(data['phi1']) + 0.000001 #0 + 0.0000001 # TODO: pq sumar?
     phi2 = float(data['phi2']) + 0.000001 #0
     phidelta = float(data['phidelta']) #0.1
@@ -67,40 +78,61 @@ def experiment(data, folder):
 
     logging.debug("in exp1", locals())
 
+    # open FreeCAD document
     FreeCAD.openDocument(freecad_file)
     doc = FreeCAD.ActiveDocument
 
+    # create materials
     raytrace.create_reflector_lambertian_layer("rlamb", 1.0)
     raytrace.create_two_layers_material("RLamb", "rlamb", "rlamb")
-
-    file_Si = PV_file #'Perovs.txt'  # change for your path
+    file_Si = PV_file
     raytrace.create_PV_material("PV1", file_Si)
 
+    # prepare Scene
     sel = doc.Objects
     current_scene = raytrace.Scene(sel)
     ##### data_file_spectrum = 'ASTMG173-direct.txt'
     # light_spectrum = raytrace.create_CDF_from_PDF(data_file_spectrum)
     # Buie_model = raytrace.BuieDistribution(0.05)
 
-    destfolder = folder + '.output'
-    os.makedirs(destfolder)
-    outfile = open(os.path.join(destfolder,'kkk4.txt'), 'w')
-    outfile_PV = open(os.path.join(destfolder,'PV-10000-CAS4-kk.txt'), 'w')
-    outfile_PV_values = open(os.path.join(destfolder,'PV_values_1micro.txt'), 'w')
-    outfile_Source_lambdas = open(os.path.join(destfolder,'Source_lambdas_1micro.txt'), 'w')
-    outfile_Source_lambdas.write("%s %s" % (aperture_collector * 0.001 * 0.001, "# Collector aperture in m2") + '\n')
-    outfile_Source_lambdas.write("%s %s" % (number_of_rays, "# Rays per wavelength") + '\n')
-    outfile_Source_lambdas.write("%s %s" % (lambdadelta, "# Step of wavelength in nm") + '\n')
 
-
-    pool = multiprocessing.Pool()
-    list_of_args = []
-    logging.debug("number of cpus: %s", multiprocessing.cpu_count())
+    # Prepare input for multiprocessing
+    list_pars = []
     for ph in np.arange(phi1, phi2, phidelta):
         for th in np.arange(theta1, theta2, thetadelta):
             for w in np.arange(lambda1, lambda2, lambdadelta):
-                list_of_args.append((ph, th, w, number_of_rays, aperture_collector))
-    pool.map_async(compute,list_of_args, callback=process_computation)
+                #pool.apply_async(compute,(ph, th, w, number_of_rays, aperture_collector))#, callback=process_computation)
+                #logging.debug("pooling %s, %s, %s",ph,th,w)
+                list_pars.append((ph, th, w, number_of_rays, aperture_collector))
+    global total_computations
+    total_computations = len(list_pars)
+    ###list_pars = [par+(total_computations,) for par in list_pars]
+
+    # Prepare shared counter of finished computations
+    finished_computations_counter = multiprocessing.Value('i',0)
+
+    # Prepare pool of workers and feed it
+    logging.debug("number of cpus: %s", multiprocessing.cpu_count())
+    pool = multiprocessing.Pool(initializer = init_counter, initargs = (finished_computations_counter, ))
+    results = pool.map(compute, list_pars)
+    logging.debug('finisehd pool.map %s, %s', len(results), len(list_pars))
+
+    # Close document
+    FreeCAD.closeDocument(doc.Name)
+
+    # Process results
+    Source_lambdas = []
+    PV_energy = []
+    PV_wavelength = []
+    PV_values = []
+    efficiencies = []
+
+    for result in results:
+        (ph, th, w, efficiency, pv_energy, pv_wavelength, pv_values) = result
+        PV_energy.append(pv_energy)
+        PV_wavelength.append(pv_wavelength)
+        PV_values.append(pv_values)
+        Source_lambdas.append(w)
 
     xarray = np.array(np.concatenate(PV_energy))
     yarray = np.array(np.concatenate(PV_wavelength))
@@ -108,13 +140,22 @@ def experiment(data, folder):
     datacomp = datacomp.T
     data_PV_values = np.array(np.concatenate(PV_values))
     data_Source_lambdas = np.array(Source_lambdas)
-    np.savetxt(outfile_PV, datacomp, fmt=['%f', '%f'])
-    np.savetxt(outfile_PV_values, data_PV_values, fmt=['%f', '%f', '%f', '%f', '%f', '%f', '%f', '%f', '%f', '%f'])
 
-    np.savetxt(outfile_Source_lambdas, data_Source_lambdas, fmt=['%f'])
+    # Write files with output
+    destfolder = folder + '.output'
+    os.makedirs(destfolder)
+    with open(os.path.join(destfolder,'kkk4.txt'), 'w') as outfile:
+        for result in results:
+            outfile.write(str(result)+'\n')
+    with open(os.path.join(destfolder,'PV-10000-CAS4-kk.txt'), 'w') as outfile_PV:
+        np.savetxt(outfile_PV, datacomp, fmt=['%f', '%f'])
+    with open(os.path.join(destfolder,'PV_values_1micro.txt'), 'w') as outfile_PV_values:
+        np.savetxt(outfile_PV_values, data_PV_values, fmt=['%f', '%f', '%f', '%f', '%f', '%f', '%f', '%f', '%f', '%f'])
+    with open(os.path.join(destfolder,'Source_lambdas_1micro.txt'), 'w') as outfile_Source_lambdas:
+        outfile_Source_lambdas.write("%s %s" % (aperture_collector * 0.001 * 0.001, "# Collector aperture in m2") + '\n')
+        outfile_Source_lambdas.write("%s %s" % (number_of_rays, "# Rays per wavelength") + '\n')
+        outfile_Source_lambdas.write("%s %s" % (lambdadelta, "# Step of wavelength in nm") + '\n')
+        np.savetxt(outfile_Source_lambdas, data_Source_lambdas, fmt=['%f'])
 
-    outfile.close()
-    outfile_PV.close()
-    outfile_PV_values.close()
-    outfile_Source_lambdas.close()
+    # Prepare zipfile
     shutil.make_archive(folder, 'zip', destfolder)
