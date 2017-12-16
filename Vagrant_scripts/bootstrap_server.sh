628021505dfc5915fb8bcb408928906849647244
Biel Cardona <bielcardona@gmail.com>
2017-12-16 11:49:51 +0100

    chmod a server file

--- Vagrant_scripts/bootstrap_server.sh
+++ Vagrant_scripts/bootstrap_server.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 cp /vagrant/server_files/mysites.conf /etc/apache2/sites-available/
+chmod 644 /etc/apache2/sites-available/mysites.conf
 a2ensite mysites
 a2dissite 000-default
 service apache2 reload
