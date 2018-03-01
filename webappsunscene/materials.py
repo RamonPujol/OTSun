062d685e785a610022bcfaf24f1d3aaaa1b3feeb
Biel Cardona <bielcardona@gmail.com>
2018-03-01 21:32:10 +0100

    formatting and typos

--- webappsunscene/materials.py
+++ webappsunscene/materials.py
@@ -3,6 +3,7 @@ import logging
 import sys
 import os
 from uuid import uuid4
+
 sys.path.append("/usr/lib/freecad")
 sys.path.append("/usr/lib/freecad/lib")
 import FreeCAD
@@ -11,7 +12,7 @@ import numpy as np
 import time
 import dill
 
-logger=logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 UPLOAD_FOLDER = '/tmp/WebAppSunScene'
 if not os.path.exists(UPLOAD_FOLDER):
@@ -22,13 +23,14 @@ else:
         UPLOAD_FOLDER += str(uuid4())
         os.makedirs(UPLOAD_FOLDER)
 
-def create_material(data,files):
+
+def create_material(data, files):
     logger.debug(data)
     kind_of_material = data['kind_of_material']
     if kind_of_material == 'constant_ior':
         raytrace.create_simple_volume_material(data['name'],
-                                                    float(data['ior']),
-                                                    None if data['ex_co'] == None else float(data['ex_co']))
+                                               float(data['ior']),
+                                               None if data['ex_co'] == '' else float(data['ex_co']))
     elif kind_of_material == 'variable_ior':
         raytrace.create_wavelength_volume_material(data['name'], files['ior_file'])
     elif kind_of_material == 'PV_volume':
@@ -48,18 +50,18 @@ def create_material(data,files):
                                                 float(data['c_constant']))
     elif kind_of_material == 'reflector_specular_layer':
         raytrace.create_reflector_specular_layer(data['name'],
-                                                    float(data['por']),
-                                                    None if data['sigma_1'] == '' else float(data['sigma_1']),
-                                                    None if data['sigma_2'] == '' else float(data['sigma_2']),
-                                                    None if data['k'] == '' else float(data['k']))
+                                                 float(data['por']),
+                                                 None if data['sigma_1'] == '' else float(data['sigma_1']),
+                                                 None if data['sigma_2'] == '' else float(data['sigma_2']),
+                                                 None if data['k'] == '' else float(data['k']))
     elif kind_of_material == 'reflector_lambertian_layer':
         raytrace.create_reflector_lambertian_layer(data['name'], float(data['por']))
     elif kind_of_material == 'metallic_specular_layer':
         raytrace.create_metallic_specular_layer(data['name'],
-                                                    files['ior_file'],
-                                                    None if data['sigma_1'] == '' else float(data['sigma_1']),
-                                                    None if data['sigma_2'] == '' else float(data['sigma_2']),
-                                                    None if data['k'] == '' else float(data['k']))
+                                                files['ior_file'],
+                                                None if data['sigma_1'] == '' else float(data['sigma_1']),
+                                                None if data['sigma_2'] == '' else float(data['sigma_2']),
+                                                None if data['k'] == '' else float(data['k']))
     elif kind_of_material == 'polarized_coating_reflector_layer':
         raytrace.create_polarized_coating_reflector_layer(data['name'], files['coating_file'])
     elif kind_of_material == 'polarized_coating_transparent_layer':
@@ -78,11 +80,11 @@ def create_material(data,files):
         raytrace.create_reflector_lambertian_layer("rlamb", float(data['por']))
         raytrace.create_two_layers_material(data['name'], "rlamb", "rlamb")
     elif kind_of_material == 'simple_absorber_surface':
-        raytrace.create_absorber_simple_material(data['name'], 1-float(data['poa']))
+        raytrace.create_absorber_simple_layer(data['name'], 1 - float(data['poa']))
     material = raytrace.Material.by_name[data['name']]
     temp_folder = os.path.join(UPLOAD_FOLDER, str(uuid4()))
     os.makedirs(temp_folder)
-    filename = os.path.join(temp_folder, data['name']+'.rtmaterial')
+    filename = os.path.join(temp_folder, data['name'] + '.rtmaterial')
     # filename = '/tmp/'+data['name']+'.rtmaterial'
     with open(filename, 'wb') as f:
         dill.dump(material, f)
