a225da5c056949daa47196e81ed8c13d2f03f624
Biel Cardona <bielcardona@gmail.com>
2018-07-26 08:46:26 +0200

    app name is configurable

--- webappsunscene/default_settings.py
+++ webappsunscene/default_settings.py
@@ -3,8 +3,9 @@
 # 2. Create an environment variable OTSUN_CONFIG_FILE pointing to the file
 # 3. Make this environment variable visible to the wsgi process that calls the app
 #    (see http://ericplumb.com/blog/passing-apache-environment-variables-to-django-via-mod_wsgi.html)
-UPLOAD_FOLDER = '/tmp/WebAppSunScene'
+UPLOAD_FOLDER = '/tmp/OTSunDevelopmentServer'
 MAIL_SENDER = 'no@no.com'
 MAIL_SERVER = 'smtp.no.com'
 MAIL_PASSWD = 'secret'
 MAIL_PORT = 0
+APP_NAME = 'OTSun Development Server'
