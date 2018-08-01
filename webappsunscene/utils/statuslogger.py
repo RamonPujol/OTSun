c7529d1fcefa5ec78fe142214ce930eb554eec30
Biel Cardona <bielcardona@gmail.com>
2018-08-01 10:01:37 +0200

    refactored and added docstrings to statuslogger

--- webappsunscene/utils/statuslogger.py
+++ webappsunscene/utils/statuslogger.py
@@ -2,46 +2,80 @@ from __future__ import print_function
 
 import os
 import json
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class StatusLogger(object):
-    def __init__(self, manager, total, root_folder):
+    """
+    StatusLogger keeps information about the status of a computation.
+
+    Attributes:
+        val: current value
+        lock: lock for access to data
+        total: number of interations to complete the computations
+        folder: location where to store the status.log file
+        filename: filename where to store information
+        data: current status
+    """
+    def __init__(self, manager, total, folder):
+        """
+        Inits Status logger with the given parameters
+        Args:
+            manager: a multiprocessing.Manager object used for locking
+            total: the number of iterations to make to complete the process
+            folder: the folder where to store the status.log file
+        """
         self.val = manager.Value('i', 0)
         self.lock = manager.Lock()
         self.total = total
-        self.root_folder = root_folder
-        self.status_file = os.path.join(root_folder, 'status.json')
-        self.data_status = {'status': 'started', 'percentage': 'N/A'}
-        self.save_status()
+        self.folder = folder
+        self.filename = os.path.join(folder, 'status.json')
+        self.data = {'status': 'started', 'percentage': 'N/A'}
+        self.save()
 
     def increment(self):
+        """
+        Increases self.val value in one unit and updates the status if required
+        """
         with self.lock:
             self.val.value += 1
             value = self.val.value
             if (value == self.total) or ((value % (int(self.total / 100)+1)) == 0):
-                self.update_data()
+                logger.debug("updating at %d", value)
+                self.update()
             else:
-                print("not updating at {}".format(value))
+                logger.debug("not updating at %d", value)
 
     def value(self):
+        """
+        Returns the current value of the counter
+        """
         with self.lock:
             return self.val.value
 
-    def update_data(self):
+    def update(self):
+        """
+        Updates the data dictionary and saves the result in the file
+        """
         # print("updating data {}".format(self.data_status))
         if self.val.value == self.total:
             status = 'finished'
         else:
             status = 'running'
         percentage = (100*self.val.value)/self.total
-        self.data_status['status'] = status
-        self.data_status['percentage'] = percentage
-        self.save_status()
+        self.data['status'] = status
+        self.data['percentage'] = percentage
+        self.save()
 
-    def save_status(self):
-        print("saving data: {}".format(self.data_status))
-        with open(self.status_file, 'w') as fh:
-            json.dump(self.data_status, fh)
+    def save(self):
+        """
+        Saves the status data in the self.filename
+        """
+        logger.info("saving data: %s", self.data)
+        with open(self.filename, 'w') as fh:
+            json.dump(self.data, fh)
 
 
 if __name__ == '__main__':
@@ -49,6 +83,7 @@ if __name__ == '__main__':
     import random
     import time
 
+    logging.basicConfig(level=logging.DEBUG)
 
     def f(args):
         counter = args[1]
