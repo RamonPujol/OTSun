9294a8320b436438faf8721b506b8cc8c18f7e50
Gabriel Cardona <bielcardona@gmail.com>
2019-12-11 10:12:24 +0100

    improved startup scripts

--- Vagrant_scripts/local_server.sh
+++ Vagrant_scripts/local_server.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
-# add-apt-repository -y ppa:freecad-maintainers/freecad-stable
-apt update
-apt -y install python-pip
+## add-apt-repository -y ppa:freecad-maintainers/freecad-legacy
+add-apt-repository -y ppa:freecad-maintainers/freecad-stable
+apt-get update
+# apt-get install -y freecad
+apt-get install -y libfreecad-python3-0.18
+# apt-get install -y python-enum34
+DEBIAN_FRONTEND=noninteractive apt-get install -y python3-pip
+# pip install autologging
+# pip3 install autologging
+pip3 install pytest
+mkdir -p /home/vagrant/.local/lib/python3.6/site-packages
+echo "/usr/lib/freecad-python3/lib" > /home/vagrant/.local/lib/python3.6/site-packages/freecad.pth
 git clone https://github.com/bielcardona/OTSun.git
-apt -y install freecad
-pip install -e OTSun/
-pip install -e /vagrant/
-
+pip3 install -e OTSun/
+pip3 install -e /vagrant/
+## pip install dill==0.2.7.1
