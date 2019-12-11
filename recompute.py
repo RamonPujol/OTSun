86e17671c580395cc1c779865d95c15b27658a5c
Gabriel Cardona <bielcardona@gmail.com>
2019-12-11 13:29:13 +0100

    modified recompute.py

--- recompute.py
+++ recompute.py
@@ -10,15 +10,20 @@ def get_dir():
     this_dir = os.path.dirname(os.path.abspath(__file__))
     upload_dir = os.path.join(this_dir,'computations')
     if not os.path.exists(upload_dir):
-        os.makedirs(upload_dir)
+        sys.exit("This script is in the wrong folder")
+        # os.makedirs(upload_dir)
     else:
         if not os.access(upload_dir, os.W_OK):
-            upload_dir += str(uuid4())
-            os.makedirs(upload_dir)
+            sys.exit("Cannot access this folder")
+            # upload_dir += str(uuid4())
+            # os.makedirs(upload_dir)
     return upload_dir
 
 if __name__ == '__main__':
     folder = get_dir()
-    ident = sys.argv[1]
+    try:
+        ident = sys.argv[1]
+    except:
+        sys.exit("usage: python recompute.py identifier")
     app.config.from_mapping(UPLOAD_FOLDER = folder)
     run_offline(ident)
