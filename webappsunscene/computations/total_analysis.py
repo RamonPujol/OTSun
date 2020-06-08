15f5967f9f33986686ecb1bb7deb65cc7b35f58f
Gabriel Cardona <bielcardona@gmail.com>
2020-06-08 21:30:52 +0200

    total analysis now with parallelization

--- webappsunscene/computations/total_analysis.py
+++ webappsunscene/computations/total_analysis.py
@@ -6,27 +6,98 @@ sys.path.append("/usr/lib/freecad/lib")
 import FreeCAD
 import otsun
 import numpy as np
-import multiprocessing
+import time
+from multiprocessing import Queue, Process, Manager, active_children
 from webappsunscene.utils.statuslogger import StatusLogger
 
 logger = logging.getLogger(__name__)
+data_consumer_queue = Queue()
+n_cpu = 20
+
+def data_consumer():
+    data = []
+    while True:
+        m = data_consumer_queue.get()
+        if m == 'kill':
+            logger.info("Finished getting results")
+            break
+        logger.info(f"appending {m}")
+        data.append(m)
+    data.sort()
+
+    power_emitted_by_m2 = otsun.integral_from_data_file(data_file_spectrum)
+
+    logger.info("Writing files")
+    with open(os.path.join(destfolder, 'efficiency_results.txt'), 'w') as outfile_efficiency_results:
+        outfile_efficiency_results.write(
+            "%s %s" % (aperture_collector_Th * 0.001 * 0.001, "# Collector Th aperture in m2") + '\n')
+        outfile_efficiency_results.write(
+            "%s %s" % (aperture_collector_PV * 0.001 * 0.001, "# Collector PV aperture in m2") + '\n')
+        outfile_efficiency_results.write("%s %s" % (power_emitted_by_m2, "# Source power emitted by m2") + '\n')
+        outfile_efficiency_results.write("%s %s" % (number_of_rays, "# Rays emitted")+ '\n')
+        outfile_efficiency_results.write("%s" % (
+            "#phi theta efficiency_from_source_Th efficiency_from_source_PV") + '\n')
+        for result in data:
+            outfile_efficiency_results.write("%.3f %.3f %.6f %.6f\n" % result)
+
+
+def compute(*args):
+    ph, th, statuslogger = args
+
+    main_direction = otsun.polar_to_cartesian(ph, th) * -1.0  # Sun direction vector
+
+    if move_elements:
+        tracking = otsun.MultiTracking(main_direction, current_scene)
+        tracking.make_movements()
+
+    emitting_region = otsun.SunWindow(current_scene, main_direction)
+    l_s = otsun.LightSource(current_scene, emitting_region, light_spectrum, 1.0, direction_distribution,
+                            polarization_vector)
+
+    exp = otsun.Experiment(current_scene, l_s, number_of_rays, show_in_doc)
+    logger.info("launching experiment %s", [os.getpid(), ph, th, main_direction])
+    try:
+        exp.run()
+    except:
+        logger.error("computation ended with an error")
+        return
+    if aperture_collector_Th != 0.0:
+        efficiency_from_source_th = (exp.captured_energy_Th / aperture_collector_Th) / (
+                exp.number_of_rays / exp.light_source.emitting_region.aperture)
+    else:
+        efficiency_from_source_th = 0.0
+    if aperture_collector_PV != 0.0:
+        efficiency_from_source_pv = (exp.captured_energy_PV / aperture_collector_PV) / (
+                exp.number_of_rays / exp.light_source.emitting_region.aperture)
+    else:
+        efficiency_from_source_pv = 0.0
+    data_consumer_queue.put((ph, th, efficiency_from_source_th, efficiency_from_source_pv))
+
+    if move_elements:
+        tracking.undo_movements()
+
+    statuslogger.increment()
+
 
 def computation(data, root_folder):
-    global doc
+    # global doc
     global current_scene
 
-    manager = multiprocessing.Manager()
+    manager = Manager()
     statuslogger = StatusLogger(manager, 0, root_folder)
 
     logger.info("experiment from total_analysis got called")
     _ROOT = os.path.abspath(os.path.dirname(__file__))
+    global data_file_spectrum
     data_file_spectrum = os.path.join(_ROOT, 'data', 'ASTMG173-direct.txt')
+    global destfolder
     destfolder = os.path.join(root_folder, 'output')
     try:
         os.makedirs(destfolder)
     except:
         pass  # we suppose it already exists
 
+    global polarization_vector
     polarization_vector = None
     phi_ini = float(data['phi_ini']) + 1.E-9
     phi_end = float(data['phi_end']) + 1.E-4
@@ -42,13 +113,16 @@ def computation(data, root_folder):
     else:
         theta_step = float(data['theta_step'])
 
+    global number_of_rays
     number_of_rays = int(data['numrays'])
 
+    global aperture_collector_PV
     if data['aperture_pv'] == "":
         aperture_collector_PV = 0
     else:
         aperture_collector_PV = float(data['aperture_pv'])
 
+    global aperture_collector_Th
     if data['aperture_th'] == "":
         aperture_collector_Th = 0
     else:
@@ -58,6 +132,7 @@ def computation(data, root_folder):
     # Inputs for Total Analysis
     # ---
     # for direction of the source two options: Buie model or main_direction
+    global direction_distribution
     if data['CSR'] == "":
         direction_distribution = None # default option main_direction
     else:
@@ -65,6 +140,7 @@ def computation(data, root_folder):
         Buie_model = otsun.buie_distribution(CSR)
         direction_distribution = Buie_model
 
+    global light_spectrum
     light_spectrum = otsun.cdf_from_pdf_file(data_file_spectrum)
 
     files_folder = os.path.join(root_folder, 'files')
@@ -79,66 +155,40 @@ def computation(data, root_folder):
     current_scene = otsun.Scene(sel)
 
 
-    number_of_runs = 0
-    for _ in np.arange(phi_ini, phi_end, phi_step):
-        for _ in np.arange(theta_ini, theta_end, theta_step):
-            number_of_runs += 1
-
-    move_elements = data.get('move_scene', 'no') == 'yes'
-
-
-    statuslogger.total = number_of_runs
-    show_in_doc = None
-    results = []
+    list_pars = []
     for ph in np.arange(phi_ini, phi_end, phi_step):
         for th in np.arange(theta_ini, theta_end, theta_step):
-            main_direction = otsun.polar_to_cartesian(ph, th) * -1.0  # Sun direction vector
+            list_pars.append((ph, th, statuslogger))
 
-            if move_elements:
-                tracking = otsun.MultiTracking(main_direction, current_scene)
-                tracking.make_movements()
+    global move_elements
+    move_elements = data.get('move_scene', 'no') == 'yes'
 
-            emitting_region = otsun.SunWindow(current_scene, main_direction)
-            l_s = otsun.LightSource(current_scene, emitting_region, light_spectrum, 1.0, direction_distribution,
-                                       polarization_vector)
+    statuslogger.total = len(list_pars)
+    global show_in_doc
+    show_in_doc = None
 
-            exp = otsun.Experiment(current_scene, l_s, number_of_rays, show_in_doc)
-            logger.info("launching experiment %s", [ph, th, main_direction])
-            try:
-                exp.run()
-            except:
-                logger.error("computation ended with an error")
-                continue
-            if aperture_collector_Th != 0.0:
-                efficiency_from_source_th = (exp.captured_energy_Th /aperture_collector_Th) / (
-                        exp.number_of_rays/exp.light_source.emitting_region.aperture)
-            else:
-                efficiency_from_source_th = 0.0
-            if aperture_collector_PV != 0.0:
-                efficiency_from_source_pv = (exp.captured_energy_PV /aperture_collector_PV) / (
-                        exp.number_of_rays/exp.light_source.emitting_region.aperture)
-            else:
-                efficiency_from_source_pv = 0.0
-            results.append((ph, th, efficiency_from_source_th, efficiency_from_source_pv))
+    data_consumer_process = Process(target=data_consumer)
+    data_consumer_process.start()
+    remaining = list_pars[:]
+    processes = []
+    while remaining:
+        free_slots = n_cpu - len(active_children())
+        # print(f"{free_slots} free slots")
+        process_now = remaining[:free_slots]
+        remaining = remaining[free_slots:]
+        for args in process_now:
+            p = Process(target=compute, args=args)
+            p.start()
+            processes.append(p)
+        time.sleep(0.1)
+    logger.info("All tasks queued")
+    for p in processes:
+        p.join()
+    # for ch in active_children():
+    #     if ch != data_consumer_process:
+    #         logger.info(f"Joining {ch.pid}")
+    #         ch.join()
+    logger.info("Putting poison")
+    data_consumer_queue.put('kill')
+    data_consumer_process.join()
 
-            if move_elements:
-                tracking.undo_movements()
-
-            statuslogger.increment()
-
-    if move_elements:
-        tracking.make_movements()
-
-    power_emitted_by_m2 = otsun.integral_from_data_file(data_file_spectrum)
-
-    with open(os.path.join(destfolder, 'efficiency_results.txt'), 'w') as outfile_efficiency_results:
-        outfile_efficiency_results.write(
-            "%s %s" % (aperture_collector_Th * 0.001 * 0.001, "# Collector Th aperture in m2") + '\n')
-        outfile_efficiency_results.write(
-            "%s %s" % (aperture_collector_PV * 0.001 * 0.001, "# Collector PV aperture in m2") + '\n')
-        outfile_efficiency_results.write("%s %s" % (power_emitted_by_m2, "# Source power emitted by m2") + '\n')
-        outfile_efficiency_results.write("%s %s" % (number_of_rays, "# Rays emitted")+ '\n')
-        outfile_efficiency_results.write("%s" % (
-            "#phi theta efficiency_from_source_Th efficiency_from_source_PV") + '\n')
-        for result in results:
-            outfile_efficiency_results.write("%.3f %.3f %.6f %.6f\n" % result)
